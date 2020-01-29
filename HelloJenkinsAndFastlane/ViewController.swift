ed8a8b8ec02272c8750c82f41125d8377ecfa824
Бадисова Виктория Дмитриевна <viktoriia.badisova@fsight.ru>
2020-01-29 11:44:43 +0300

    initial

---
+++ HelloJenkinsAndFastlane/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloJenkinsAndFastlane
+//
+//  Created by Виктория Бадисова on 29/01/2020.
+//  Copyright © 2020 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
